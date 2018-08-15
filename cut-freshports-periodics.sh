5d5ae79d5c2c7c38a2def9f19578b3d08f6c293a
Dan Langille <dan@langille.org>
2018-08-15 14:20:12 +0000

    Fix URLS
    git-svn-id: svn://127.0.0.16/freshports-1/packaging/trunk@5101 46e82423-29d8-e211-989e-002590a4cdd4

--- cut-freshports-periodics.sh
+++ cut-freshports-periodics.sh
@@ -3,11 +3,11 @@
 VERSION=$1
 MSG=$2
 
-svn copy svn+ssh://dan@svn.int.unixathome.org/usr/local/svn/repos/freshports-1/periodics/trunk \
-         svn+ssh://dan@svn.int.unixathome.org/usr/local/svn/repos/freshports-1/periodics/tags/${VERSION} -m "'${MSG}'"
+svn copy svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/periodics/trunk \
+         svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/periodics/tags/${VERSION} -m "'${MSG}'"
 
 cd ~/tmp
-svn export svn+ssh://dan@svn.int.unixathome.org/usr/local/svn/repos/freshports-1/periodics/tags/${VERSION} freshports-periodics-${VERSION}
+svn export svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/periodics/tags/${VERSION} freshports-periodics-${VERSION}
 
 tar -czf freshports-periodics-${VERSION}.tar.gz freshports-periodics-${VERSION}
 scp freshports-periodics-${VERSION}.tar.gz r710-01:
