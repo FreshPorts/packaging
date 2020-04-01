374fccc831f76866368316c1737ef1ce6e0f5bfe
Dan Langille <dan@langille.org>
2020-04-01 20:50:39 +0000

    Update to new locations
    git-svn-id: svn://127.0.0.16/freshports-1/packaging/trunk@5269 46e82423-29d8-e211-989e-002590a4cdd4

--- cut-fp-listen.sh
+++ cut-fp-listen.sh
@@ -3,11 +3,11 @@
 VERSION=$1
 MSG=$2
 
-svn copy svn+ssh://dan@svn.int.unixathome.org/usr/local/svn/repos/freshports-1/daemontools/trunk/fp-listen \
-         svn+ssh://dan@svn.int.unixathome.org/usr/local/svn/repos/freshports-1/daemontools/tags/fp-listen-${VERSION} -m "'${MSG}'"
+svn copy svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/trunk/fp-listen \
+         svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/tags/fp-listen-${VERSION} -m "'${MSG}'"
 
 cd ~/tmp
-svn export svn+ssh://dan@svn.int.unixathome.org/usr/local/svn/repos/freshports-1/daemontools/tags/fp-listen-${VERSION} freshports-fp-listen-${VERSION}
+svn export svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/tags/fp-listen-${VERSION} freshports-fp-listen-${VERSION}
 
 tar -czf freshports-fp-listen-${VERSION}.tar.gz freshports-fp-listen-${VERSION}
 scp freshports-fp-listen-${VERSION}.tar.gz pkg01:
