000b65a2413063ae83fc69ee8f034b3f86704f47
Dan Langille <dan@langille.org>
2021-01-09 03:08:28 +0000

    Add
    git-svn-id: svn://127.0.0.16/freshports-1/packaging/trunk@5571 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ cut-fp-listen-git.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+VERSION=$1
+MSG=$2
+
+svn copy svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/branches/git/fp-listen \
+         svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/tags/fp-listen-git-${VERSION} -m "'${MSG}'"
+
+cd ~/tmp
+svn export svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/tags/fp-listen-git-${VERSION} freshports-fp-listen-git-${VERSION}
+
+tar -czf freshports-fp-listen-git-${VERSION}.tar.gz freshports-fp-listen-git-${VERSION}
+scp freshports-fp-listen-git-${VERSION}.tar.gz pkg01.int.unixathome.org:
