81d3c8944af4e0e0db0e6f7326026bc20b3ac884
Dan Langille <dan@langille.org>
2021-06-20 09:58:32 +0000

    start using ~/tmp/packaging
    git-svn-id: svn://127.0.0.16/freshports-1/packaging/trunk@5722 46e82423-29d8-e211-989e-002590a4cdd4

--- cut-fp-listen.sh
+++ cut-fp-listen.sh
@@ -6,7 +6,7 @@ MSG=$2
 svn copy svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/trunk/fp-listen \
          svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/tags/fp-listen-${VERSION} -m "'${MSG}'"
 
-cd ~/tmp
+cd ~/tmp/packaging/
 svn export svn+ssh://svnusers@svn.int.unixathome.org/freshports-1/daemontools/tags/fp-listen-${VERSION} freshports-fp-listen-${VERSION}
 
 tar -czf freshports-fp-listen-${VERSION}.tar.gz freshports-fp-listen-${VERSION}
